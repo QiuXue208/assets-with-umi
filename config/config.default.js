3386bfa32864dd1f99a3408d793c9acf150af959
Charles <1309571292@qq.com>
2021-06-08 14:25:38 +0800

    模板渲染

--- config/config.default.js
+++ config/config.default.js
@@ -1,7 +1,7 @@
 /* eslint valid-jsdoc: "off" */
 
 'use strict';
-
+const path = require('path');
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
@@ -17,6 +17,14 @@ module.exports = appInfo => {
 
   // add your middleware config here
   config.middleware = [];
+
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    root: path.join(appInfo.baseDir, 'app/view'),
+    mapping: {
+      '.html': 'nunjucks',
+    },
+  };
 
   // add your user config here
   const userConfig = {
